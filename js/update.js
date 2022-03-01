37cf97358886d87fd009d227547b1653df9f05a7
moritz.albach@t-online.de <moritz.albach@t-online.de>
2022-03-01 17:09:19 +0100

    checkneighbours() funktioniert in randbereichen

--- js/update.js
+++ js/update.js
@@ -1,11 +1,11 @@
 // das Gesamte Board updaten und jeden Gme tic ausgeführt werden 
 export function update(board){ 
-    const board_size = board.length-2 
+    const board_size = board.length 
 
     //Durchläuft das Gesamte Board array und ruft für jede Zelle checkneighbours() auf
-    for(var i = 1; i< board_size; i++){
-        for(var j = 1; j < board_size; j++){            
-            for(var l = 1; l < board_size; l++){
+    for(var i = 0; i< board_size; i++){
+        for(var j = 0; j < board_size; j++){            
+            for(var l = 0; l < board_size; l++){
                 checkneighbours(i, j, l)
             }
         }
@@ -17,17 +17,16 @@ export function update(board){
             for(var o = - 1; o < 2; o++){
                 for(var p = - 1; p < 2; p++){
                     
-
-                    if(board[i+k][j+o][l+p].alive == 1 ){
-                        if( (i+k != i) || (j+p != j) || (l+o != l)){
-                           
-                            board[i][j][l].neighbours += 1
-                            console.log("fuck ja")
+                    if((i+k != -1 && i+k != board_size) && (j+o != -1 && j+o != board_size) && (l+p != -1 && l+p != board_size)){// checkt ob die abfrage außerhalb des board
+                       // console.log(i+k +", " + (j+o) + ", " + (l+p))
+                        if(board[i+k][j+o][l+p].alive == 1 ){
+                            if( (i+k != i) || (j+p != j) || (l+o != l)){// sorgt dafür, das sich die Zelle selbst als nachbar erkennt.
+                               
+                                board[i][j][l].neighbours += 1
+                                console.log("fuck ja")
+                            }
                         }
-                        
                     }
-                        
-                    
                 }
             }
         }
